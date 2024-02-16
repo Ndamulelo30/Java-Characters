24b0d0ba433b77aa4831956adbbdb0883127f0df
Ndamulelo30 <141257791+Ndamulelo30@users.noreply.github.com>
2024-02-16 14:49:07 +0200

    Add files via upload

---
+++ 13.JavaCharacters/src/main/java/com/mycompany/javacharacters/App.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javacharacters;
+
+/**
+ *
+ * @author Sanele
+ */
+public class App {
+
+    public static void main(String[] args) {
+        char myGrade = 'B';
+        System.out.println(myGrade);
+        
+        String greeting = "Hello World";
+        System.out.println(greeting);
+    }
+}
